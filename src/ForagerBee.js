dfeb4862055f37bdfb48c4dd4a5d72d78452a6d6
James Powers <powersajames@gmail.com>
2018-01-22 11:12:40 -0800

    completed 6ees6ees6ees

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,14 @@
-class ForagerBee {
-  // TODO..
+class ForagerBee extends Bee{
+	constructor(color) {
+		super(color);
+		this.age = 10;
+		this.job = 'find pollen';
+		this.canFly = true;
+		this.treasureChest = [];
+	}
+
+	forage(treasure) {
+		this.treasureChest.push(treasure);
+	}
+	
 };
